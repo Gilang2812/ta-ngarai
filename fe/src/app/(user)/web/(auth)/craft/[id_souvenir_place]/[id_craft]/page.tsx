81a3a21ac2daedc05dd301691db3b23f745f98dd
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2025-08-19 17:32:59 +0700

    feat: Add ProductDetailPage component for displaying craft details

---
+++ fe/src/app/(user)/web/(auth)/craft/[id_souvenir_place]/[id_craft]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { SingleContentWrapper } from "@/components/common/SingleContentWrapper";
+import ProductDetail from "@/components/craft/ProdukDetail";
+
+export default async function ProductDetailPage({
+  params,
+}: {
+  params: Promise<{ id_craft: string; id_souvenir_place: string }>;
+}) {
+  const { id_craft, id_souvenir_place } = await params;
+  return (
+    <SingleContentWrapper>
+      <ProductDetail id_craft={id_craft} id_souvenir_place={id_souvenir_place} />
+    </SingleContentWrapper>
+  );
+}
