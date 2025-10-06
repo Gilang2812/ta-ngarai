946ebef96916011c22e567b2204a470e8a5cb334
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2025-10-06 17:17:48 +0700

    fix: update environment variable names for database connection to ensure correct configuration

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,9 @@
 // require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
-let user = process.env.USER;
-let password = process.env.PASS;
-let host = process.env.HOST;
+let user = process.env.DB_USER;
+let password = process.env.DB_PASS;
+let host = process.env.DB_HOST;
 let db_name = process.env.DB_NAME;
 let db = process.env.DB;
 
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(
 );
 try {
   sequelize.authenticate();
-  console.log(user, password, host, db_name,db);
+  console.log(user, password, host, db_name, db);
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
