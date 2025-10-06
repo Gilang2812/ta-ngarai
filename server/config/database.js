971a7bca77ffe19c96295659ca0b91c3c8e3cfba
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2025-10-06 17:09:33 +0700

    style: format code for consistency and readability in database configuration

--- server/config/database.js
+++ server/config/database.js
@@ -1,20 +1,21 @@
 // require("dotenv").config();
 
-const { Sequelize } = require('sequelize')
-let user = process.env.USER
-let password = process.env.PASS
-let host = process.env.HOST
-let db_name = process.env.DB_NAME
-let db  = process.env.DB
+const { Sequelize } = require("sequelize");
+let user = process.env.USER;
+let password = process.env.PASS;
+let host = process.env.HOST;
+let db_name = process.env.DB_NAME;
+let db = process.env.DB;
 
 const sequelize = new Sequelize(
   `${db}://${user}:${password}@${host}/${db_name}`
-)
+);
 try {
-   sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+  sequelize.authenticate();
+  console.log(user, password, host, db_name,db);
+  console.log("Connection has been established successfully.");
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to the database:", error);
 }
 
-module.exports = sequelize
+module.exports = sequelize;
