8d7e761d336b9219aa68351008a966ad0faf6404
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2025-10-06 19:44:43 +0700

    fix: set default values for database connection parameters

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,20 @@
 // require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
-let user = process.env.DB_USER;
-let password = process.env.DB_PASS;
-let host = process.env.DB_HOST;
-let db_name = process.env.DB_NAME;
-let db = process.env.DB;
+let user = process.env.DB_USER || 'root';
+let password = process.env.DB_PASS || '';
+let host = process.env.DB_HOST || 'localhost';
+let port = process.env.DB_PORT || 3306;
+let db_name = process.env.DB_NAME || 'kotogadang';
+let db = process.env.DB || 'mysql';
+
 
 const sequelize = new Sequelize(
-  `${db}://${user}:${password}@${host}/${db_name}`
+  `${db}://${user}:${password}@${host}:${port}/${db_name}`
 );
 try {
   sequelize.authenticate();
-  console.log(user, password, host, db_name, db);
+  console.log(user, password, host, port, db_name, db);
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
