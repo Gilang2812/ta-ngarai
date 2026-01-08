18033bdab57aa2e105f19378186fcc75d120385f
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2026-01-08 10:06:01 +0700

    fix: remove trailing slash from imageUrl for consistency

--- fe/src/lib/baseUrl.ts
+++ fe/src/lib/baseUrl.ts
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
-export const imageUrl = `${baseUrl}/images/`;
+export const imageUrl = `${baseUrl}/images`;
 export const imageNotFound = "/images/notFound/imageNotFound.jpg";
 export const imageLoading = "/images/loading/Spinner.gif";
 export const notFoundImage = "/images/Image_not_available.png";
